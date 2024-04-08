43316e7239b7e19e7329bf4d970f7ed7efcedce2
cecgithub <ceciliasimplon@gmail.com>
2024-04-08 09:40:49 +0200

    reception front

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/backoffice', name: 'app_backoffice')]
+    public function backoffice(): Response
+    {
+        return $this->render('backoffice.html.twig', [
+        ]);
+    }
+}
